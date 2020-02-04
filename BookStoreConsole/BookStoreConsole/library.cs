09e2b9a982e25578e721069bc344a6fa3067cd32
jerilynm <jerilynm@microsoft.com>
2020-02-03 16:28:59 -0800

    commit

---
+++ BookStoreConsole/BookStoreConsole/library.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BookStoreConsole
+{
+    class library 
+    {
+        #region Properties
+        /// <summary>
+        /// Book Store Items
+        /// </summary>
+        /// 
+        /// Book Titles
+        public string BookTitle { get; set; }
+        /// <summary>
+        /// Book Author
+        /// </summary>
+        public string BookAuthor { get; set; }
+        /// <summary>
+        /// Book Cost
+        /// </summary>
+        public decimal BookCost { get; set; }
+        /// <summary>
+        /// Book type Hard or Soft
+        /// </summary>
+        public string BookType { get; set; }
+        /// <summary>
+        /// Book Categories, Fiction, Non fiction
+        /// </summary>
+        public string BookCategory { get; set; }
+    #endregion
+
+
+    #region Methods
+    /// My Methods Find books
+    /// 
+    public string
+
+        #endregion
+
+    }
+
+}
